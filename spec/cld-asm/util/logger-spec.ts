95aa13adedbd1b350d69669fa7bfc4b783552d02
OJ Kwon <kwon.ohjoong@gmail.com>
2017-08-25 21:10:05 -0700

    test(util): add test cases for utility

---
+++ spec/cld-asm/util/logger-spec.ts
@@ -0,0 +1,29 @@
+//tslint:disable:no-require-imports
+import { expect } from 'chai';
+import loggerType = require('../../../src/util/logger');
+
+describe('logger', () => {
+  let log: typeof loggerType.log;
+  let enableLogger: typeof loggerType.enableLogger;
+
+  beforeEach(() => {
+    ({ log, enableLogger } = require('../../../src/util/logger'));
+  });
+
+  it('should do nothing by default', () => {
+    expect(() => log('')).to.not.throw();
+  });
+
+  it('should allow override logger', () => {
+    const mock = jest.fn();
+    enableLogger(mock);
+
+    const message = 'message';
+    const value = { value: 'value' };
+
+    log(message, value);
+
+    expect(mock.mock.calls).to.have.lengthOf(1);
+    expect(mock.mock.calls[0]).to.deep.equal([message, value]);
+  });
+});
