cc98f9e320f9a4e5c2bebb96e8d302e358839d1a
OJ Kwon <kwon.ohjoong@gmail.com>
2017-11-29 15:25:51 -0800

    feat(logger): enablelogger appends scope

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -14,8 +14,12 @@ const log: logFunctionType = (...args: Array<any>) => (logInstance as any)(...ar
  * @param logger function to log.
  */
 const enableLogger = (logger: logFunctionType) => {
-  logInstance = logger;
-  emscriptenEnableLogger(logger);
+  const scopedLogger = (scope: string) => (message: string, ...optionalParams: Array<any>) => {
+    logger(`${scope}::${message}`, ...optionalParams);
+  };
+
+  logInstance = scopedLogger(`cld3`);
+  emscriptenEnableLogger(scopedLogger(`cld3Loader`));
 };
 
 export { enableLogger, logFunctionType, log };
