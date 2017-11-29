d7ab905a0d6499d302b4b8133e8b69a63558979c
OJ Kwon <kwon.ohjoong@gmail.com>
2017-11-29 15:28:33 -0800

    feat(loadmodule): support single file binary load
    BREAKING CHANGE: now runs on native-wasm supported runtime only

--- src/loadModule.ts
+++ src/loadModule.ts
@@ -1,34 +1,28 @@
-import { ENVIRONMENT, isWasmEnabled } from 'emscripten-wasm-loader';
+import { ENVIRONMENT, getModuleLoader } from 'emscripten-wasm-loader';
+import { CldAsmModule } from './cldAsmModule';
 import { CldFactory } from './cldFactory';
-import { getLoader } from './getLoader';
+import { cldLoader } from './cldLoader';
 import { log } from './util/logger';
 
 /**
  * Load, initialize wasm / asm.js binary to use actual cld wasm instances.
  *
- * @param {binaryEndpoint} [string] For overring path to wasm binary on node.js or browser.
  * @param {environment} [ENVIRONMENT] For overriding running environment
  *
- * @returns {Promise<CldFactory>} Factory function of cld to allow create instance of cld.
+ * @returns {(environment?: ENVIRONMENT) => Promise<CldFactory>} Function to load module
  */
-const loadModule: (binaryEndpoint?: string, environment?: ENVIRONMENT) => Promise<CldFactory> = async (
-  binaryEndpoint?: string,
-  environment?: ENVIRONMENT
-) => {
-  const binaryPath = `./lib/${isWasmEnabled() ? 'wasm' : 'asm'}`;
+const loadModule = async (environment?: ENVIRONMENT) => {
+  log(`loadModule: loading cld3 module`);
 
-  try {
-    return await getLoader(binaryPath, binaryEndpoint, environment);
-  } catch (e) {
-    log(`loadModule: cannot load module from `, binaryPath);
+  //imports MODULARIZED emscripten preamble
+  const runtimeModule = require(`./lib/cld3`); //tslint:disable-line:no-require-imports no-var-requires
 
-    if (!isWasmEnabled()) {
-      throw e;
-    } else {
-      log(`loadModule: try to fallback to asm.js runtime`);
-      return await getLoader(`./lib/asm`, binaryEndpoint, environment);
-    }
-  }
+  const moduleLoader = await getModuleLoader<CldFactory, CldAsmModule>(
+    (runtime: CldAsmModule, env: ENVIRONMENT) => cldLoader(runtime, env),
+    runtimeModule
+  );
+
+  return moduleLoader(environment);
 };
 
 export { loadModule };
