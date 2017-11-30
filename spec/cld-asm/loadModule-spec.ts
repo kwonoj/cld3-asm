f07aa89e37371799e73054d9eb5f3d2724a978e2
OJ Kwon <kwon.ohjoong@gmail.com>
2017-11-29 16:43:28 -0800

    refactor(test): migrate into mock hoisting

--- spec/cld-asm/loadModule-spec.ts
+++ spec/cld-asm/loadModule-spec.ts
@@ -1,83 +1,38 @@
-//tslint:disable:no-require-imports
 import { expect } from 'chai';
-import loadModuleType = require('../../src/loadModule');
-
-//we're mocking emscripten-wasm-loader, can't import values from there.
-//create value stub for assertion.
-enum mockENVIRONMENT {
-  NODE = 'NODE',
-  WEB = 'WEB'
-}
+import { ENVIRONMENT } from 'emscripten-wasm-loader';
+import { loadModule } from '../../src/loadModule';
 
 jest.mock('../../src/lib/cld3', () => jest.fn(), { virtual: true });
+jest.mock('../../src/cldLoader');
+jest.mock('emscripten-wasm-loader', () => ({
+  isWasmEnabled: jest.fn(),
+  isNode: jest.fn(),
+  getModuleLoader: jest.fn(),
+  ENVIRONMENT: {
+    WEB: 'WEB',
+    NODE: 'NODE'
+  }
+}));
+
+const { getModuleLoader: getModuleLoaderMock } = require('emscripten-wasm-loader'); //tslint:disable-line:no-require-imports no-var-requires
 
 describe('loadModule', () => {
-  let loadModule: typeof loadModuleType.loadModule;
-  let mockGetModuleLoader: jest.Mock<any>;
-
-  beforeEach(() => {
-    mockGetModuleLoader = jest.fn(() => jest.fn());
-    jest.mock('emscripten-wasm-loader', () => ({
-      isWasmEnabled: jest.fn(),
-      isNode: jest.fn(),
-      getModuleLoader: jest.fn((cb: Function) => {
-        cb();
-        return mockGetModuleLoader;
-      }),
-      ENVIRONMENT: mockENVIRONMENT
-    }));
-
-    loadModule = require('../../src/loadModule').loadModule;
-  });
-
   it('should create moduleLoader on browser environment override', async () => {
-    await loadModule(mockENVIRONMENT.WEB as any);
+    const mockModuleLoader = jest.fn();
+    getModuleLoaderMock.mockImplementationOnce((cb: Function) => {
+      cb();
+      return mockModuleLoader;
+    });
+    await loadModule(ENVIRONMENT.WEB);
 
-    const getModuleLoaderCalls = (require('emscripten-wasm-loader').getModuleLoader as jest.Mock<any>).mock.calls;
-    expect(getModuleLoaderCalls).to.have.lengthOf(1);
-
-    const moduleLoaderCalls = mockGetModuleLoader.mock.calls;
-    expect(moduleLoaderCalls).to.have.lengthOf(1);
-    expect(moduleLoaderCalls[0]).to.deep.equal([mockENVIRONMENT.WEB]);
+    expect(mockModuleLoader.mock.calls[0]).to.deep.equal([ENVIRONMENT.WEB]);
   });
 
   it('should create module on node environmnet override', async () => {
-    await loadModule(mockENVIRONMENT.NODE as any);
+    const mockModuleLoader = jest.fn();
+    getModuleLoaderMock.mockReturnValueOnce(mockModuleLoader);
+    await loadModule(ENVIRONMENT.NODE);
 
-    const getModuleLoaderCalls = (require('emscripten-wasm-loader').getModuleLoader as jest.Mock<any>).mock.calls;
-    expect(getModuleLoaderCalls).to.have.lengthOf(1);
-
-    const moduleLoaderCalls = mockGetModuleLoader.mock.calls;
-    expect(moduleLoaderCalls).to.have.lengthOf(1);
-    expect(moduleLoaderCalls[0]).to.deep.equal([mockENVIRONMENT.NODE]);
-  });
-
-  it('should create moduleLoader on browser environment', async () => {
-    const { isNode } = require('emscripten-wasm-loader');
-    (isNode as jest.Mock<any>).mockReturnValueOnce(false);
-
-    await loadModule();
-
-    const getModuleLoaderCalls = (require('emscripten-wasm-loader').getModuleLoader as jest.Mock<any>).mock.calls;
-    expect(getModuleLoaderCalls).to.have.lengthOf(1);
-
-    const moduleLoaderCalls = mockGetModuleLoader.mock.calls;
-    expect(moduleLoaderCalls).to.have.lengthOf(1);
-    expect(moduleLoaderCalls[0]).to.deep.equal([undefined]);
-  });
-
-  it('should create module on node environmnet', async () => {
-    const { isNode } = require('emscripten-wasm-loader');
-    (isNode as jest.Mock<any>).mockReturnValueOnce(true);
-
-    await loadModule();
-
-    const getModuleLoaderCalls = (require('emscripten-wasm-loader').getModuleLoader as jest.Mock<any>).mock.calls;
-    expect(getModuleLoaderCalls).to.have.lengthOf(1);
-
-    const moduleLoaderCalls = mockGetModuleLoader.mock.calls;
-    expect(moduleLoaderCalls).to.have.lengthOf(1);
-    expect(moduleLoaderCalls[0]).to.deep.equal([undefined]);
+    expect(mockModuleLoader.mock.calls[0]).to.deep.equal([ENVIRONMENT.NODE]);
   });
 });
-//tslint:enable:no-require-imports
