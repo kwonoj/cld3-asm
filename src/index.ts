4a6ecdce9f4f05e8a39629f0a20b9b71349270dd
OJ Kwon <kwon.ohjoong@gmail.com>
2019-06-13 12:12:07 -0700

    feat(loadmodule): no longer accepts environment
    BREAKING CHANGES: do not support environment override

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,3 @@ export { log, enableLogger } from './util/logger';
 export { LanguageIdentifier, CldFactory } from './cldFactory';
 export { LanguageResult } from './cldAsmModule';
 export { LanguageCode } from './languageCode';
-export { ENVIRONMENT } from 'emscripten-wasm-loader';
--- src/loadModule.ts
+++ src/loadModule.ts
@@ -1,8 +1,11 @@
-import { ENVIRONMENT, getModuleLoader, isNode } from 'emscripten-wasm-loader';
+import { getModuleLoader } from 'emscripten-wasm-loader';
 import { CldAsmModule } from './cldAsmModule';
 import { CldFactory } from './cldFactory';
 import { cldLoader } from './cldLoader';
 import { log } from './util/logger';
+
+//imports MODULARIZED emscripten preamble
+import * as runtime from './lib/node/cld3';
 
 /**
  * Load, initialize wasm binary to use actual cld wasm instances.
@@ -10,7 +13,6 @@ import { log } from './util/logger';
  * @param [InitOptions] Options to initialize cld3 wasm binary.
  * @param {number} [InitOptions.timeout] - timeout to wait wasm binary compilation & load.
  * @param {string | object} [InitOptions.locateBinary] - custom resolution logic for wasm binary. (not supported)
- * @param {ENVIRONMENT} [InitOptions.environment] For overriding running environment
  * It could be either remote endpoint url, or loader-returned object for bundler. Check examples/browser_* for references.
  *
  * @returns {() => Promise<CldFactory>} Function to load module
@@ -18,27 +20,16 @@ import { log } from './util/logger';
 const loadModule = async (
   initOptions: Partial<{
     timeout: number;
-    environment?: ENVIRONMENT;
   }> = {}
 ) => {
-  //imports MODULARIZED emscripten preamble
-  //tslint:disable-next-line:no-require-imports no-var-requires
-  const runtime = require(`./lib/cld3`);
-
-  const { environment, timeout } = initOptions;
-  const env = environment ? environment : isNode() ? ENVIRONMENT.NODE : ENVIRONMENT.WEB;
+  const { timeout } = initOptions;
 
   log(`loadModule: loading cld3 wasm binary`, { initOptions });
 
-  //https://github.com/kwonoj/docker-hunspell-wasm/issues/63
-  //Build module object to construct wasm binary module via emscripten preamble.
-  //apply overridden environment values to custom patched hunspell preamble.
-  const overriddenModule = { ENVIRONMENT: env };
-
   const moduleLoader = await getModuleLoader<CldFactory, CldAsmModule>(
-    (runtime: CldAsmModule) => cldLoader(runtime, env),
+    (runtime: CldAsmModule) => cldLoader(runtime),
     runtime,
-    overriddenModule,
+    undefined,
     { timeout }
   );
 
